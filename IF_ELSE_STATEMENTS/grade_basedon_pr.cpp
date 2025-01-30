5df6c1eeac5839f0798361d30fbc86f3abbbb6f6
prashantdesale259 <prashantdesale259@gmail.com>
2025-01-30 16:17:46 +0530

    complated

--- IF_ELSE_STATEMENTS/grade_basedon_pr.cpp
+++ IF_ELSE_STATEMENTS/grade_basedon_pr.cpp
@@ -5,20 +5,24 @@ int main (){
 	int percentage;
 	cout << "enter yore percentage :";
 	cin >> percentage;
-	if(percentage> 90 && percentage <=100){
-		cout <<" your grad is A+ !";
+	
+	if(percentage >= 100 || percentage < 0){
+ 	cout << "Enter Valid Percentage : ";
+    }
+	else if(percentage> 90 && percentage <=100){
+		cout <<" your grad is A !";
 	}
 	else if(percentage> 75 && percentage <=89){
-		cout << "your grad is A !" ;
+		cout << "your grad is : B !" ;
 	}
-	else if(percentage> 55 && percentage <=74){
-		cout << "your grad is B !" ;
+	else if(percentage> 51 && percentage <=74){
+		cout << "your grad is : C !" ;
 	}
-	else if(percentage> 33 && percentage <=55){
-		cout << "your grad is C !" ;
+	else if(percentage> 33 && percentage <=50){
+		cout << "your grad is : D !" ;
 	}
 	else{
-		cout << "you are failed, please give reExam..." ;
+		cout << "you are failed, Batter luck next time..." ;
 	}
 	
 	return 0;
